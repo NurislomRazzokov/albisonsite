ff2b3c744a29de7d5358de7c1f7356fe0c35908f
ismoiljondev <115632460+ismoiljondev@users.noreply.github.com>
2024-01-19 08:37:38 +0500

    a

--- app/components/CookieBanner.tsx
+++ app/components/CookieBanner.tsx
@@ -28,9 +28,9 @@ export default function CookieBanner() {
     <div
       className={`my-10 mx-auto max-w-max md:max-w-screen-sm
                         fixed bottom-0 left-0 right-0 
-                        ${cookieConsent != null ? "hidden" : "flex"}
+                     ${cookieConsent != null ? "hidden" : "flex"}
                  px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
-                         bg-gray-700 rounded-lg shadow`}
+                         bg rounded-lg shadow-2xl`}
     >
       <div className="text-center">
         <Link href="/info/cookies">
@@ -43,13 +43,13 @@ export default function CookieBanner() {
 
       <div className="flex gap-2">
         <button
-          className="px-5 py-2 text-gray-300 rounded-md border-gray-900"
+          className="px-5 py-2 text-white rounded-md border-black border"
           onClick={() => setCookieConsent(false)}
         >
           Decline
         </button>
         <button
-          className="bg-gray-900 px-5 py-2 text-white rounded-lg"
+          className="bg-black px-5 py-2 text-white rounded-lg"
           onClick={() => setCookieConsent(true)}
         >
           Allow Cookies
