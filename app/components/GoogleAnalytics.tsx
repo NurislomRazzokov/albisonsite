12122654d49347504633dd1e8caf34ad57065b1d
Ismoiljondev <ismoiljonhalilov5@gmail.com>
2024-01-31 08:55:23 +0500

    test

--- app/components/GoogleAnalytics.tsx
+++ app/components/GoogleAnalytics.tsx
@@ -1,34 +1,35 @@
 // components/GoogleAnalytics.tsx
-"use client";
-
-import Script from "next/script";
-import { usePathname, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-import { pageview } from "@/app/lib/gtagHelper";
+"use client"
+import Script from "next/script"
+import { usePathname, useSearchParams } from "next/navigation"
+import { useEffect } from "react"
+import { pageview } from "@/app/lib/gtagHelper"
 export default function GoogleAnalytics({
-  GA_MEASUREMENT_ID,
+	GA_MEASUREMENT_ID,
 }: {
-  GA_MEASUREMENT_ID: string;
+	GA_MEASUREMENT_ID: string
 }) {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
+	const pathname = usePathname()
+	const searchParams = useSearchParams()
 
-  useEffect(() => {
-    const url = pathname + searchParams.toString();
+	useEffect(() => {
+		const url = pathname + searchParams.toString()
 
-    pageview(GA_MEASUREMENT_ID, url);
-  }, [pathname, searchParams, GA_MEASUREMENT_ID]);
-  return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+		pageview(GA_MEASUREMENT_ID, url)
+	}, [pathname, searchParams, GA_MEASUREMENT_ID])
+	return (
+		<>
+			<Script
+				async={true}
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+			/>
+			<Script
+				async={true}
+				id="google-analytics"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
@@ -41,8 +42,8 @@ export default function GoogleAnalytics({
                     page_path: window.location.pathname,
                 });
                 `,
-        }}
-      />
-    </>
-  );
+				}}
+			/>
+		</>
+	)
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,32 @@
-import "./globals.css";
-import Navbar from "./components/Navbar/index";
-import Footer from "./components/Footer/index";
-import Head from "next/head";
-import type { Metadata } from "next";
-import GoogleAnalytics from "@/app/components/GoogleAnalytics";
-import CookieBanner from "@/app/components/CookieBanner";
+import "./globals.css"
+import Navbar from "./components/Navbar/index"
+import Footer from "./components/Footer/index"
+import Head from "next/head"
+import type { Metadata } from "next"
+import GoogleAnalytics from "@/app/components/GoogleAnalytics"
+import CookieBanner from "@/app/components/CookieBanner"
 export const metadata: Metadata = {
-  title: "Albison Academy",
-  icons: {
-    icon: "/logo.svg",
-  },
-  description:
-    "Toshkentda professional IT treyning markazi | Albison - 2020 yildan beri o'z faoliyatini yurg'izib kelmoqda. IT Kurslar boshidan boshlab o'rgatiladi va ishga kirguncha qo’llab-quvvatlanadi. Online o'qish imkoniyati ham bor. Bepul ochiq darslar",
-};
+	title: "Albison Academy",
+	icons: {
+		icon: "/logo.svg",
+	},
+	description:
+		"Toshkentda professional IT treyning markazi | Albison - 2020 yildan beri o'z faoliyatini yurg'izib kelmoqda. IT Kurslar boshidan boshlab o'rgatiladi va ishga kirguncha qo’llab-quvvatlanadi. Online o'qish imkoniyati ham bor. Bepul ochiq darslar",
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode
 }) {
-  return (
-    <>
-      <html>
-        <GoogleAnalytics GA_MEASUREMENT_ID="G-93WN9M8Y8G" />
-        <Head>
-          <link rel="icon" href="/logo.svg" />
-          {/* Google Analytics tracking code */}
-          {/* <script
+	return (
+		<>
+			<html>
+				<GoogleAnalytics GA_MEASUREMENT_ID="G-93WN9M8Y8G" />
+				<Head>
+					<link rel="icon" href="/logo.svg" />
+					{/* Google Analytics tracking code */}
+					{/* <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-93WN9M8Y8G"
           ></script>
@@ -42,16 +42,16 @@ export default function RootLayout({
             `,
             }}
           ></script> */}
-        </Head>
-        <body>
-          <Navbar />
-          {children}
-          <Footer />
-          <CookieBanner />
-        </body>
-      </html>
-    </>
-  );
+				</Head>
+				<body>
+					<Navbar />
+					{children}
+					<Footer />
+					<CookieBanner />
+				</body>
+			</html>
+		</>
+	)
 }
 
 // <!-- Google tag (gtag.js) -->
